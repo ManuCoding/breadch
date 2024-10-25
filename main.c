42de1b9906fcd318f8e18a4bba0d5281626b9c23
ManuCoding <manucaouette@gmail.com>
2024-10-25 00:22:57 -0400

    Removed ncurses, started to read config

--- main.c
+++ main.c
@@ -1,26 +1,45 @@
-#include <ncurses.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <termios.h>
+
+#define MAX_LINE_LENGTH 1024
+
+char** read_crumbs(size_t* count) {
+	FILE* f=fopen("~/.breadcrumbs","r");
+	if(!f) {
+		printf("No set breadcrumb\n");
+	}
+
+	char** paths=NULL;
+	char buf[MAX_LINE_LENGTH];
+	*count=0;
+	return paths;
+}
+
+typedef struct termios Termios;
+Termios initial_state={0};
+
+void revert_state() {
+	tcsetattr(0,TCSAFLUSH,&initial_state);
+}
 
 int main() {
-	initscr();
-	cbreak();
-	noecho();
+	tcgetattr(0,&initial_state);
+	atexit(revert_state);
 
-	int y,x;
-	getyx(stdscr,y,x);
+	Termios raw=initial_state;
+	raw.c_lflag&=~(ICANON|ECHO); // Disable canonical mode and echo
+	tcsetattr(STDIN_FILENO,TCSAFLUSH,&raw);
 
-	int win_width=COLS;
-	int win_height=5;
+	int ch=0;
+	while(ch!='q') {
+		ch=getchar();
+		printf("%c",ch);
+	}
+	printf("\n");
 
-	WINDOW* win=newwin(win_height,win_width,y+1,0);
-	box(win,0,0);
-	mvwprintw(win,1,1,"Testing...");
-
-	mvprintw(1,1,"okokok");
-	wrefresh(win);
-	refresh();
-	getch();
-
-	delwin(win);
-	endwin();
 	return 0;
 }
+
