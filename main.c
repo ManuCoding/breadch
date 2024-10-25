ca854791bbddbfe401f50c5b081ff2f001b959bc
ManuCoding <manucaouette@gmail.com>
2024-10-25 13:07:32 -0400

    Successfully reading config file, leaking memory

--- main.c
+++ main.c
@@ -7,14 +7,34 @@
 #define MAX_LINE_LENGTH 1024
 
 char** read_crumbs(size_t* count) {
-	FILE* f=fopen("~/.breadcrumbs","r");
+	char* home=getenv("HOME");
+	FILE* f=NULL;
+	if(home) {
+		char path[1024];
+		snprintf(path,sizeof(path),"%s/%s",home,".breadcrumbs");
+		f=fopen(path,"r");
+	}
 	if(!f) {
 		printf("No set breadcrumb\n");
+		return NULL;
 	}
 
-	char** paths=NULL;
+	char** paths=malloc(64*sizeof(size_t));
 	char buf[MAX_LINE_LENGTH];
 	*count=0;
+	while(fgets(buf,MAX_LINE_LENGTH,f)) {
+		paths[*count]=malloc(MAX_LINE_LENGTH);
+		memcpy(paths[*count],buf,MAX_LINE_LENGTH);
+		char* line=paths[*count];
+		for(size_t i=0; i<MAX_LINE_LENGTH; i++) {
+			if(line[i]=='\n' || line[i]=='\0') {
+				line[i]='\0';
+				i=MAX_LINE_LENGTH;
+			}
+		}
+		line[MAX_LINE_LENGTH-1]='\0';
+		*count+=1;
+	}
 	return paths;
 }
 
@@ -26,6 +46,12 @@ void revert_state() {
 }
 
 int main() {
+	size_t count=0;
+	char** crumbs=read_crumbs(&count);
+	for(size_t i=0; i<count; i++) {
+		printf("%s\n",crumbs[i]);
+	}
+	return 0;
 	tcgetattr(0,&initial_state);
 	atexit(revert_state);
 
